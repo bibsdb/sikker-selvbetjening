2b9624793a0f30a9c1a7aac611fef186233d296c
LimeSlime11 <bartoszbrozio@gmail.com>
2026-04-15 08:27:56 +0200

    updated files

--- build_files/10-packages.sh
+++ build_files/10-packages.sh
@@ -7,16 +7,9 @@ set -ouex pipefail
 dnf5 install -y \
 	tmux \
 	gnome-shell-extension-dash-to-panel \
-	glibc-langpack-da \
-	libreoffice \
-	libreoffice-langpack-da \
-	hunspell-da 
 
 # Use a COPR Example:
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
 # Disable COPRs so they do not end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-
-#glibc-langpack-da and libreoffice-langpack-da are used to get danish language support in the image.
-#hunspell-da is used by LibreOffice for spell checking in danish.
