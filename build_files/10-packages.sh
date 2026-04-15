0ef2bc9e6ce22eac5368f477984b9a8863946d16
LimeSlime11 <bartoszbrozio@gmail.com>
2026-04-15 09:31:51 +0200

    Added arcmenu,libreoffice,languagepack

--- build_files/10-packages.sh
+++ build_files/10-packages.sh
@@ -5,11 +5,23 @@ set -ouex pipefail
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images.
 dnf5 install -y \
-	tmux \
 	gnome-shell-extension-dash-to-panel \
-
+	gnome-shell-extension-arcmenu \
+	glibc-langpack-da \
+	glibc-locale-source \
+	libreoffice \
+    libreoffice-langpack-da
 # Use a COPR Example:
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
 # Disable COPRs so they do not end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+#list of what each package does respectively:
+
+# gnome-shell-extension-dash-to-panel: windows-like taskbar for gnome
+# gnome-shell-extension-arcmenu: windows-like start menu for gnome
+# glibc-langpack-da: Danish language support for glibc
+# glibc-locale-source: source files for glibc locales, needed to generate da_DK.UTF-8 locale
+# libreoffice: office suite
+# libreoffice-langpack-da: Danish language support for LibreOffice
