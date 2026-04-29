cc773fe9a48a8b014946e2eea45243e3b73effc7
Agnete Moos <agms@sonderborg.dk>
2026-04-29 13:44:46 +0200

    Remove desktop background script

--- build_files/25-desktop-background.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ouex pipefail
-
-# Install compile helper from system_files into the final image.
-install -Dm0755 \
-	/ctx/system_files/usr/libexec/sikker-compile-desktop-background \
-	/usr/libexec/sikker-compile-desktop-background
