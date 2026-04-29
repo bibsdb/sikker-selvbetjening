68599dd3445d493a25ea198e2a33ce8321f0c5f0
Agnete Moos <agms@sonderborg.dk>
2026-04-29 09:25:10 +0200

    Fix path error

--- build_files/15-system-defaults.sh
+++ build_files/15-system-defaults.sh
@@ -2,14 +2,16 @@
 
 set -ouex pipefail
 
-# Install image-level system defaults so they apply even without Anaconda.
-install -Dm0644 /ctx/system_files/etc/hostname /etc/hostname
-install -Dm0644 /ctx/system_files/etc/locale.conf /etc/locale.conf
-install -Dm0644 /ctx/system_files/etc/vconsole.conf /etc/vconsole.conf
-install -Dm0644 /ctx/system_files/etc/X11/xorg.conf.d/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
-ln -snf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime
+# Install image-level defaults in /usr/etc so bootc/ostree can merge them into
+# the target system without mutating runtime-managed files during the build.
+install -d /usr/etc
+install -Dm0644 /ctx/system_files/etc/hostname /usr/etc/hostname
+install -Dm0644 /ctx/system_files/etc/locale.conf /usr/etc/locale.conf
+install -Dm0644 /ctx/system_files/etc/vconsole.conf /usr/etc/vconsole.conf
+install -Dm0644 /ctx/system_files/etc/X11/xorg.conf.d/00-keyboard.conf /usr/etc/X11/xorg.conf.d/00-keyboard.conf
+ln -snf /usr/share/zoneinfo/Europe/Copenhagen /usr/etc/localtime
 
 # Suppress GNOME Initial Setup even on direct bootc-based installs.
-install -d /etc/systemd/system /etc/systemd/user
-ln -snf /dev/null /etc/systemd/system/gnome-initial-setup.service
-ln -snf /dev/null /etc/systemd/user/gnome-initial-setup-first-login.service
+install -d /usr/etc/systemd/system /usr/etc/systemd/user
+ln -snf /dev/null /usr/etc/systemd/system/gnome-initial-setup.service
+ln -snf /dev/null /usr/etc/systemd/user/gnome-initial-setup-first-login.service
