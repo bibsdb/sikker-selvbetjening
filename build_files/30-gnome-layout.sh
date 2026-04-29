b2c5cdd8efc572d24da1124c8dd6abf059defa89
Agnete Moos <agms@sonderborg.dk>
2026-04-29 16:38:12 +0200

    Install dconf profile and defaults

--- build_files/30-gnome-layout.sh
+++ build_files/30-gnome-layout.sh
@@ -2,7 +2,9 @@
 
 set -ouex pipefail
 
-# Copy all dconf defaults
+# Install dconf profile and defaults.
+install -d /etc/dconf/profile /etc/dconf/db/local.d
+install -Dm0644 /ctx/system_files/etc/dconf/profile/user /etc/dconf/profile/user
 cp -r /ctx/system_files/etc/dconf/db/local.d/* /etc/dconf/db/local.d/
 
 dconf update
